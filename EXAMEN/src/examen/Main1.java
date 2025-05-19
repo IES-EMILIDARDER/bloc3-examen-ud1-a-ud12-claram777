abfca65f9a511d6906a901929e0e7368386816d1
claram777 <cmp00@iesemilidarder.com>
2025-05-19 15:56:39 +0200

    Examen

--- EXAMEN/src/examen/Main1.java
+++ EXAMEN/src/examen/Main1.java
@@ -1,8 +1,58 @@
 package examen;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 public class Main1 {
     
     public static void main(String[] args) {
+        /*
+        De la llista de vehicles anterior, 
+        tria els posteriors a l'any 2014 i 
+        obté una llista ordenada (de major a menor) de les diferents 'Marca' 
+        RESULTAT: 
+        [Seat, Tesla, Volkswagen]
+        */
+        
+        try (Stream<String> linies = Files.lines(Paths.get("c:\\temp\\vehicles.csv"))) {
+            List<Vehicle> v1 = 
+                    (List<Vehicle>) linies.filter(linia -> !linia.startsWith("#") && !linia.isBlank())
+                        .map(linia -> linia.split(","))
+                        .filter(parts -> Integer.valueOf(parts[3].trim()) >= 2014)
+                        .distinct()
+                        .sorted((x, y) -> x[1].compareTo(y[1]))
+                        .map(parts -> new Vehicle (
+                                parts[0].trim(),
+                                parts[1].trim(),
+                                parts[2].trim(),
+                                Integer.valueOf(parts[3].trim()),
+                                Double.valueOf(parts[4].trim())
+                        ))
+                        .collect(Collectors.toList());
+            
+            v1.forEach(System.out::println);
+                    
+                    
+//                    linies.filter(linia -> !linia.startsWith("#") && !linia.isBlank())
+//                    .map(linia -> linia.split(","))
+//                    .sorted((x, y) -> x[1].compareTo(y[1]))
+//                    .map(parts -> new Clients (
+//                            Integer.valueOf(parts[0].trim()),
+//                            parts[1].trim(),
+//                            parts[2].trim()
+//                    ))
+//                    .collect(Collectors.toList());
+//            
+//            client.forEach(n -> System.out.println(n));
+                    
+                    
+            
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
         
     }
 }
--- EXAMEN/src/examen/Vehicle.java
+++ EXAMEN/src/examen/Vehicle.java
@@ -1,5 +1,7 @@
 package examen;
 
+import java.util.Objects;
+
 class Vehicle {
     private String matricula;
     private String marca;
@@ -14,6 +16,40 @@ class Vehicle {
         this.any = any;
         this.preu = preu;
     }
-    
 
+    public int getAny() {
+        return any;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.matricula);
+        hash = 23 * hash + Objects.hashCode(this.marca);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehicle other = (Vehicle) obj;
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        return Objects.equals(this.marca, other.marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" + "matricula=" + matricula + ", marca=" + marca + ", model=" + model + ", any=" + any + ", preu=" + preu + '}';
+    }
+    
 }
